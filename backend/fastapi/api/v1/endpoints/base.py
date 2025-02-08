1bd8f1a39d0b3b8f455da351b8dc7f017cadcd37
Gloria Lim <glorialimartt@gmail.com>
2025-02-08 00:05:50 -0500

    Update paths

--- backend/fastapi/api/v1/endpoints/base.py
+++ backend/fastapi/api/v1/endpoints/base.py
@@ -6,9 +6,9 @@ from backend.fastapi.dependencies.database import get_sync_db, Base
 from fastapi.responses import RedirectResponse
 
 
-root_router = APIRouter()
+router = APIRouter()
 
-@root_router.get("/")
+@router.get("/")
 def onboard_message():
     # Redirect to the React frontend's root URL
     return RedirectResponse(url=os.getenv("REACT_FRONTEND_URL", "http://localhost:3000"))
--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -7,8 +7,8 @@ from backend.fastapi.core.init_settings import args
 from backend.fastapi.core.middleware import setup_cors, setup_session, add_doc_protect
 from backend.fastapi.core.routers import setup_routers
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.api.v1.endpoints.base import root_router
-from fastapi.api.v1.endpoints.classroom import router as classroom_router
+from backend.fastapi.api.v1.endpoints.base import router as root_router
+from backend.fastapi.api.v1.endpoints.classroom import router as classroom_router
 
 # Initiate a FastAPI App.
 app = FastAPI()

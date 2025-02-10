5800192d0fcc4f608f304d8b2b37aab21e83658e
Asal Zooashkiany <Asal.zooashkiany@gmail.com>
2025-02-09 21:32:51 -0500

    Saved changes to routers.py before pulling main

--- backend/fastapi/core/routers.py
+++ backend/fastapi/core/routers.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from backend.fastapi.api.v1.endpoints import base, doc, classroom
+from backend.fastapi.api.v1.endpoints import base, doc, classroom ,speech_to_text
 def setup_routers(app: FastAPI):
     app.include_router(base.router, prefix="", tags=["main"])
     app.include_router(doc.router, prefix="", tags=["doc"])
     app.include_router(classroom.router,prefix="",tags=["Classroom"])
+    app.include_router(speech_to_text.router, prefix="/speech-to-text", tags=["Speech-to-Text"])

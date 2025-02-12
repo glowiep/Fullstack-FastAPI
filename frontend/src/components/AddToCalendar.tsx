49a3722a77562fbf65546299d237221533770240
Gloria Lim <glorialimartt@gmail.com>
2025-02-12 12:48:36 -0500

    Update calendar page

---
+++ frontend/src/components/AddToCalendar.tsx
@@ -0,0 +1,12 @@
+const AddToCalendar = () => {
+    return (
+        <>
+            <button className="button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none" className="svg-icon"><g stroke-width="2" stroke-linecap="round" stroke="#fff"><rect y="5" x="4" width="16" rx="2" height="16"></rect><path d="m8 3v4"></path><path d="m16 3v4"></path><path d="m4 11h16"></path></g></svg>
+                <span className="lable">Add to Calendar</span>
+            </button>
+        </>
+    )
+};
+
+export default AddToCalendar;
